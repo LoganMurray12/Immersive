dee73a2bf4504907398e35d11b9db2cc2bb120b0
LoganMurray12 <loganmurray12@live.co.uk>
2025-02-13 16:21:07 +0000

    FPS Converter

---
+++ Assets/Scripts/HMD_Manager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class HMD_Manager : MonoBehaviour
+{
+    [SerializeField] GameObject xrPlayer;
+    [SerializeField] GameObject fpsPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Using Device:" + XRSettings.loadedDeviceName);
+        if (XRSettings.isDeviceActive || XRSettings.loadedDeviceName == "OpenXR Display")
+        {
+            Debug.Log("Using device XR Player with HMD :" + XRSettings.loadedDeviceName);
+            fpsPlayer.SetActive(false);
+            xrPlayer.SetActive(true);
+        }
+        else
+        {
+            xrPlayer.SetActive(false);
+            fpsPlayer.SetActive(true);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
